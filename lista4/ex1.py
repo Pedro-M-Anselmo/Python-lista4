34ba5b371fbc398ba8fe3d77cd7ba0da8cc20d93
Pedro Matos Anselmo <pedroanselmo2005@gmail.com>
2025-03-19 22:26:55 -0300

    Lista4

---
+++ lista4/ex1.py
@@ -0,0 +1,9 @@
+n1 = int(input("digite um numero:"))
+n2 = int(input("Digite um numero:"))
+if n1<n2:
+    print(n1,"é menor")
+else:
+     if n1>n2:
+         print(n2,"é menor")
+if n1==n2:
+     print("Numeros iguais!")
---
+++ lista4/ex2.py
@@ -0,0 +1,17 @@
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um numero:"))
+if n1<n2:
+     n1 = n1+5
+     if n1<n2:
+         print(n2,"é maior")
+     if n1>n2:
+         print(n1,"é maior")
+     if n1==n2:
+         print("São iguais")
+else:
+    if n1>n2:
+        n2=n2+5
+    if n1>n2:
+        print(n1,"é maior")
+    if n1<n2:>
+    
