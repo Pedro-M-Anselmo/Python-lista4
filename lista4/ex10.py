86cdba14f11c3fa5afa2a3b68f253ffefbbb92f3
Pedro Matos Anselmo <pedroanselmo2005@gmail.com>
2025-03-27 02:27:17 -0300

    ex10

--- lista4/ex10.py
+++ lista4/ex10.py
@@ -1,10 +1,21 @@
 n1 = int(input("Digite um numero:"))
 n2 = int(input("Digite um numero"))
-if n1<10:
+if n1<n2:
     n1=n1*10
     n2=n2/2
     n1= n1+n2
-    if n1% = 0:
+    if n1 % 2 == 0:
         print("Par")
     else:
         print("Impar")
+else:
+    if n2<n1:
+        n2 = n2*10
+        n1 = n1/2
+        n2 = n1+n2
+        if n2 % 2 == 0:
+            print("Par")
+        else:
+            print("Impar")
+
+        
